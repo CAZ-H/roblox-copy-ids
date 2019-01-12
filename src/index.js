84a107ffb306d1a6cea9c5e67d569d426f587178
CaZ-H <CaZsm@outlook.com>
2019-01-12 00:59:38 -0600

    - Removed positive lookbehind for Firefox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
         id: "CopyID",
@@ -13,22 +12,25 @@ chrome.contextMenus.onClicked.addListener(function(info) {
         const URL = info.linkUrl;
 
         // General case
-        let ID = URL.match(/(?<=\/)([0-9]+)/gi);
+        let ID = URL.match(/\/([0-9]+)/gi);
+        ID ? ID = ID[0].replace('/', '') : null;
 
         // Games
         if (!ID) {
-            ID = URL.match(/(?<=PlaceId=)([0-9]+)/gi);
+            ID = URL.match(/PlaceId=([0-9]+)/gi);
+            ID ? ID = ID[0].replace(/PlaceId=/gi, '') : null;
         }
 
         // Groups
         if (!ID) {
-            ID = URL.match(/(?<=gid=)([0-9]+)/gi);
+            ID = URL.match(/gid=([0-9]+)/gi);
+            ID ? ID = ID[0].replace(/gid=/gi, '') : null;
         }
 
         // Copy to clipboard
         if (ID) {
             let tempCopyInput = document.createElement("textarea");
-            tempCopyInput.textContent = ID[0];
+            tempCopyInput.textContent = ID;
             document.body.appendChild(tempCopyInput);
             tempCopyInput.select();
             document.execCommand('copy');
